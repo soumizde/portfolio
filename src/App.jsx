5ff6017515e64e819476c2875820f9c6548b0998
Soumi De <soumizde@gmail.com>
2023-06-21 00:45:00 +0530

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 function App() {
 
   return (
