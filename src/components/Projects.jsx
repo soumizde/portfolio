b5f0c6df4b161b2ae11ec8ece6d277f547d3c4e8
Soumi De <soumizde@gmail.com>
2023-06-22 22:07:11 +0530

    projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,93 @@
 import React from 'react'
 import styled from 'styled-components';
 
+const data = [
+    "Web Development",
+    "App Development",
+    "Visual Design",
+    "UX Research",
+    "Blogging",
+];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
 `
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  padding-left: 80px;
+`
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const ListItems = styled.li`
+    font-size: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    color: transparent;
+    -webkit-text-stroke: 1px white;
+    position: relative;
+
+    ::after{
+      content:"${(props)=>props.text}";
+      position: absolute;
+      top:0;
+      left: 0;
+      color: pink;
+      width: 0px;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    &:hover{
+      ::after{
+        animation: moveText 0.5s linear both;
+
+        @keyframes moveText{
+          to{
+              width: 100%;
+          }
+        }
+      }
+    }
+`
+
+const Right = styled.div`
+ flex: 1;
+`
+
 
 const Projects = () => {
   return (
-    <Section>Projects</Section>
+    <Section>
+      <Container>
+        <Left>
+            <List>
+              {data.map((item) =>(
+                <ListItems key={item} text={item}>{item}</ListItems>
+              ))}
+              
+            </List>
+        </Left>
+        <Right>
+
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
