422c1d39a3e73b8b50aaaa8a05afd714b5a23403
Soumi De <soumizde@gmail.com>
2023-06-24 12:34:58 +0530

    About section 3D bject rendered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import Cube from './Cube'
 
 const Section = styled.div`
   height: 100vh;
@@ -69,7 +72,12 @@ const About = () => {
     <Section>
       <Container>
         <Left>
-            
+          <Canvas camera={{fov: 25, position: [5,5,5]}}>
+              <OrbitControls enableZoom={false} autoRotate={true}/>
+              <ambientLight intensity={1}/>
+              <directionalLight position={[3,2,1]}/>
+              <Cube/>
+          </Canvas>
         </Left>
         
         <Right>
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { PerspectiveCamera, RenderTexture } from '@react-three/drei'
 import { Text } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+
 
 const Cube = () => {
+  const textRef = useRef()
+  useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime)))
   return (
     <mesh>
-        <boxGeometry args={[2,2,2]}/>
+        <boxGeometry/>
         <meshStandardMaterial>
         <RenderTexture attach="map">
             <PerspectiveCamera
                makeDefault
-               position={[0,0,2]}
+               position={[0,0,5]}
            />
            <color attach="background" args={["pink"]}/>
-           <Text fontSize={1} color="#555">
+           <Text ref={textRef} fontSize={2} color="#555">
               Hello
            </Text>
        </RenderTexture>
