b3543e4e9b4cf68bd6f7b142142f733adc2aed6e
Soumi De <soumizde@gmail.com>
2023-06-29 22:12:13 +0530

    Blogging 3D Model updated

--- src/components/Blogging.jsx
+++ src/components/Blogging.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { OrbitControls, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import Lantern from './Lantern'
+import Blog from './Blog'
 
 const Blogging = () => {
   return (
     <Canvas>
         <Stage environment="city" intensity={2.5}>
-            <Lantern/>
+            <Blog/>
         </Stage>
         <OrbitControls enableZoom={false}/>
     </Canvas>
