10c196e858044afe99c46ff3f121e7418ed293d7
Soumi De <soumizde@gmail.com>
2023-06-29 23:09:56 +0530

    UX Research 3D model update

--- src/components/UXResearch.jsx
+++ src/components/UXResearch.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Stage } from '@react-three/drei'
-
+import Uxr from './Uxr'
 
 const UXResearch = () => {
   return (
-  <div>UX-R</div>
+    <Canvas>
+        <Stage environment="city" intensity={2.5}>
+            <Uxr/>
+        </Stage>
+        <OrbitControls enableZoom={false}/>
+    </Canvas>
   )
 }
 
